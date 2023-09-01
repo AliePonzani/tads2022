64142835b3f3425b56f2a5688527b640e9e57cb2
alineponzani <alinef.ponzani@gmail.com>
2023-09-01 19:40:31 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,39 +1,33 @@
 import './App.css';
 import React from 'react';
-import { Switch, Route, Link } from "react-router-dom";
+import { Link, Route, Routes } from 'react-router-dom';
 import About from './Sobre';
 import Home from './Home';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <div>
-          <nav>
-            <ul id="navigation">
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-        </Switch>
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <nav>
+          <ul id="navigation">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-    );
-  }
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
