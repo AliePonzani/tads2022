33fe14df21b8ad61d0e9cd9fb660b0f1a965cafe
alineponzani <alinef.ponzani@gmail.com>
2023-09-02 19:07:12 -0300

    alterando pagina inicial

--- src/HomePages/Home.jsx
+++ src/HomePages/Home.jsx
@@ -20,7 +20,7 @@ class Home extends React.Component {
                             EM NOSSO SITE, CADA MEMBRO DA SALA DE AULA NO CURSO DE ANÁLISE E
                             DESENVOLVIMENTO DE SISTEMAS QUE INICIARAM EM AGOSTO/2022 TEM SUA PÁGINA
                             EXCLUSIVA PARA COMPARTILHAR SUAS PAIXÕES, CONQUISTAS E PENSAMENTOS.<br /><br />
-                            EXPLORE AS HISTÓRIAS, PROJETOS E OS INTERESES DE CADA UM <Link to="/Colaboradores">CLICANDO AQUI</Link> E CONHEÇA UM POUCO MAIS SOBRE NÓS!
+                            EXPLORE AS HISTÓRIAS, PROJETOS E OS INTERESES DE CADA UM CLICANDO AQUI E CONHEÇA UM POUCO MAIS SOBRE NÓS!
                         </div>
                         <div className="Rodape">
                             <span>UM LUGAR PARA COLOCAR SUAS IDEIAS E HABILIDADES EM PRATICA</span>
