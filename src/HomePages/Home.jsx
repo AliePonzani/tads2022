3973dc3c7389de3156d83825183957fd08ae27db
alineponzani <alinef.ponzani@gmail.com>
2023-09-02 19:03:22 -0300

    inserindo pagina Home

--- src/HomePages/Home.jsx
+++ src/HomePages/Home.jsx
@@ -1,16 +1,33 @@
 import '../App.css';
+import './homeStyle.css';
 import React from 'react';
 import Imagem from '../Imagens/background.png';
 
 class Home extends React.Component {
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={Imagem} alt='Imagem'></img>
+            <div>
+                <div className="home-backgroung"></div>
+                <header>
+                    <div className="container">
+                        <div className="logo">
+                            <div><h2>TADS 2022<br />Senac</h2></div>
+                            <img src={Imagem} ></img>
+                        </div>
+                        <div className="Texto">
+                            ESTE É O LUGAR ONDE A CRIATIVIDADE E A INDIVIDUALIDADE
+                            SE ENCONTRAM.<br /><br />
+                            EM NOSSO SITE, CADA MEMBRO DA SALA DE AULA NO CURSO DE ANÁLISE E
+                            DESENVOLVIMENTO DE SISTEMAS QUE INICIARAM EM AGOSTO/2022 TEM SUA PÁGINA
+                            EXCLUSIVA PARA COMPARTILHAR SUAS PAIXÕES, CONQUISTAS E PENSAMENTOS.<br /><br />
+                            EXPLORE AS HISTÓRIAS, PROJETOS E OS INTERESES DE CADA UM <Link to="/Colaboradores">CLICANDO AQUI</Link> E CONHEÇA UM POUCO MAIS SOBRE NÓS!
+                        </div>
+                        <div className="Rodape">
+                            <span>UM LUGAR PARA COLOCAR SUAS IDEIAS E HABILIDADES EM PRATICA</span>
+                        </div>
+                    </div>
                 </header>
             </div>
-
         );
     }
 
