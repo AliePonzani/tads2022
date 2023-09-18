d148f65a01602ded839041f397cf12cf43d214e2
Aline Testes <jeiltonnunes@gmail.com>
2023-09-18 15:41:49 -0300

    Criando página AlinePonzani

---
+++ src/Paginas/AlinePonzani.jsx
@@ -0,0 +1,25 @@
+//Use esta estrutura como exemplo de como estruturar sua página
+//Navegue tbm pelas páginas Home e PaginaColaboradores para ver outros exemplos
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+//faça os imports necessários
+
+class AlinePonzani extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Testando criação de uma página!!!</h1>
+                <div>
+                    {/* Sempre deixe este link em algum lugar da sua página para servir 
+                    de rota para voltar a página com todos os Colaboradores do projeto. */}
+                    <Link to="/Colaboradores">Ver todos os Colaboradores</Link>
+                </div>
+            </div>
+            
+        );
+    }
+
+}
+
+export default AlinePonzani;
--- src/Paginas/pagesDatas.jsx
+++ src/Paginas/pagesDatas.jsx
@@ -3,12 +3,12 @@
 import CaminhoDaPagina from "./PaginaExemplo";
 import rafael from "./rafael";
 import kayky from "./kayky";
+import AlinePonzani from "./AlinePonzani";
 
 const pagesData = [
-     { name: 'Rafael Ribeiro', path: '/RafaelRibeiro', component: rafael},
-    // { name: 'Nome da Página', path: '/RotaDaPagina', component: CaminhoDaPagina},
-    // Insira aqui sua página usando a que esta acima como exemplo
+    { name: 'Rafael Ribeiro', path: '/RafaelRibeiro', component: rafael},
     {name: 'Kayky Yan', path: '/KaykyYan', component: kayky},
+    { name: 'Aline Ponzani', path: '/AlinePonzani', component: AlinePonzani},
     // { name: 'Nome da Página', path: '/RotaDaPagina', component: CaminhoDaPagina},
     // Insira aqui sua página usando a que esta acima como exemplo
 ];
